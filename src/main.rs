02b9ed04cf9fa9b06ce161ffbe2dbf110a31ff40
Samyak Gangwal <sam.gangwal97@gmail.com>
2022-07-18 23:52:27 +0530

    Updated code such that now every key press type will be detected. added .rustfmt.toml file. updated cargo.toml. Untested

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::io::{self, stdout};
+
+use termion::{event::Key, input::TermRead, raw::IntoRawMode};
+
+fn die(e: std::io::Error) {
+	panic!("{}", e);
+}
+
 fn main() {
-    println!("Hello, world!");
+	terminal::enable_raw_mode().expect("Could not turn on Raw mode");
+
+	for key in io::stdin().keys() {
+		match key {
+			Ok(key) => match key {
+				Key::Char(c) => {
+					if c.is_control() {
+						print!("{:?}\r", c as u8);
+					} else {
+						print!("{:?} ({})\r", c as u8, c);
+					}
+				}
+				Key::Ctrl('q') => break,
+				_ => println!("{:?}\r", key),
+			},
+			Err(err) => die(err),
+		}
+	}
 }
